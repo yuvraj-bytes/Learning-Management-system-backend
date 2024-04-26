8817068a100e6698f6f7e6df9f047897d49ae3bf
pallavichavda04 <pallavi.chavda@bytestechnolab.com>
2024-04-26 12:04:16 +0530

    Update:updated forgot and reset password api

--- src/api/auth/dto/reset-password.dto.ts
+++ src/api/auth/dto/reset-password.dto.ts
@@ -1,12 +1,9 @@
 import { IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class ResetPasswordDto {
-    @IsNotEmpty()
-    @IsString()
-    email: string;
 
     @IsNotEmpty()
     @IsString()
-    @MinLength(8)
+    @MinLength(6)
     newPassword: string;
 }
