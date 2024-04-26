ccf3c9fc5a1766bb30b308da95eebb7ed60f5d37
pallavichavda04 <pallavi.chavda@bytestechnolab.com>
2024-04-26 12:33:44 +0530

    Update:Updated singup and resetpassword dto

--- src/api/auth/dto/reset-password.dto.ts
+++ src/api/auth/dto/reset-password.dto.ts
@@ -1,9 +1,9 @@
-import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { IsNotEmpty, IsString, IsStrongPassword, MinLength } from 'class-validator';
 
 export class ResetPasswordDto {
-
     @IsNotEmpty()
     @IsString()
     @MinLength(6)
+    @IsStrongPassword()
     newPassword: string;
 }
